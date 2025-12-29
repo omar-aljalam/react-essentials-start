4e90e477d8965435449b9c18444b8455368a2937
Omar <omar-aljalam@outlook.com>
2025-12-29 02:38:16 +0200

    Using & Outputing Dynamic Values & Loading images

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import reactImg from "./assets/react-core-concepts.png";
+
+const reactDescription = ["Fundamental", "Crucial", "Cire"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function Header() {
+  const description = reactDescription[getRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
